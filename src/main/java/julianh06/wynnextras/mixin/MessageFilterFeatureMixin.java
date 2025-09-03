753cd24275c759e9ab31fcdb53f748f94729fdba
Teslanator20 <Rrum@gmx.de>
2025-09-03 12:25:26 +0200

    Meine aktuellen Änderungen sichern

--- src/main/java/julianh06/wynnextras/mixin/MessageFilterFeatureMixin.java
+++ src/main/java/julianh06/wynnextras/mixin/MessageFilterFeatureMixin.java
@@ -3,12 +3,14 @@ package julianh06.wynnextras.mixin;
 import com.wynntils.features.chat.MessageFilterFeature;
 import com.wynntils.handlers.chat.event.ChatMessageReceivedEvent;
 import julianh06.wynnextras.config.WynnExtrasConfig;
+import julianh06.wynnextras.features.chat.RaidChatNotifier;
 import julianh06.wynnextras.config.simpleconfig.SimpleConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.regex.Pattern;
 
 @Mixin (MessageFilterFeature.class)
 public class MessageFilterFeatureMixin {
@@ -22,13 +24,16 @@ public class MessageFilterFeatureMixin {
             config = SimpleConfig.getInstance(WynnExtrasConfig.class);
         }
 
- String msgLower = e.getStyledText().getString().toLowerCase();
+    String msgLower = e.getStyledText().withoutFormatting().getString().toLowerCase();
 
-        if (msgLower.contains("to the tower! [")) {
-                e.setCanceled(true);
-                return;
-            }
-
+         if (!msgLower.contains(":")) {
+                    for (Pattern pattern : RaidChatNotifier.BLOCKED_PATTERNS) {
+                        if (pattern.matcher(msgLower).find()) {
+                            e.setCanceled(true);
+                            return;
+                        }
+                    }
+                }
         for(String blockedWord : config.blockedWords) {
             if (msgLower.contains(blockedWord.toLowerCase())) {
                 e.setCanceled(true);
