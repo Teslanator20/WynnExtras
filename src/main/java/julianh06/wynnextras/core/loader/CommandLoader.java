c10b76e3d31d522692b89d598ce9277d75fabe5c
Teslanator20 <Rrum@gmx.de>
2025-09-06 20:45:23 +0200

    mehr zeug

--- src/main/java/julianh06/wynnextras/core/loader/CommandLoader.java
+++ src/main/java/julianh06/wynnextras/core/loader/CommandLoader.java
@@ -4,55 +4,67 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.ArgumentBuilder;
 import julianh06.wynnextras.command.ChatCommands;
 import julianh06.wynnextras.core.command.Command;
 import julianh06.wynnextras.core.command.SubCommand;
+
+
+import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
+import static com.mojang.brigadier.builder.RequiredArgumentBuilder.argument;
 
 public class CommandLoader implements WELoader {
 
     public CommandLoader() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 
-            // Basis Commands
+
             LiteralArgumentBuilder<FabricClientCommandSource> base = ClientCommandManager.literal("WynnExtras");
             LiteralArgumentBuilder<FabricClientCommandSource> alias = ClientCommandManager.literal("we");
 
-            // Chat-Command an alle anhängen
+
             base.then(ChatCommands.register());
             alias.then(ChatCommands.register());
 
-            // Chat auch als eigenes Kommando ohne Prefix
+
             dispatcher.register(ChatCommands.register());
 
-            // Eigene Commands aus COMMAND_LIST
+
             for (Command cmd : Command.COMMAND_LIST) {
                 if (cmd instanceof SubCommand) continue;
                 base.then(buildCommandTree(cmd));
                 alias.then(buildCommandTree(cmd));
             }
 
-            // Dispatcher registrieren
+
             dispatcher.register(base);
             dispatcher.register(alias);
         });
     }
 
-    private LiteralArgumentBuilder<FabricClientCommandSource> buildCommandTree(Command cmd) {
-        LiteralArgumentBuilder<FabricClientCommandSource> root = ClientCommandManager.literal(cmd.getName());
+   private LiteralArgumentBuilder<FabricClientCommandSource> buildCommandTree(Command cmd) {
+    
+       LiteralArgumentBuilder<FabricClientCommandSource> root = ClientCommandManager.literal(cmd.getName());
 
-        for (Command sub : cmd.getSubCommands()) {
-            if (sub != null) {
-                root.then(buildCommandTree(sub));
-            }
-        }
+       ArgumentBuilder<FabricClientCommandSource, ?> current = root;
 
-        for (var arg : cmd.getArguments()) {
-            if (arg != null) root.then(arg.executes(cmd::onExecute));
-        }
+       for (Command sub : cmd.getSubCommands()) {
+           if (sub != null) {
+               current = current.then(buildCommandTree(sub));
+           }
+       }
 
-        root.executes(cmd::onExecute);
-        return root;
-    }
+       for (ArgumentBuilder<FabricClientCommandSource, ?> arg : cmd.getArguments()) {
+           if (arg != null) {
+               current = current.then(arg.executes(cmd::onExecute));
+           }
+       }
+
+       current.executes(cmd::onExecute);
+
+       return root;
+   }
+
 }
 
 
